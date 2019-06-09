4c66fd8349b44ed3d4eda5e352363a0e1027ae55
Igor Kuznetsov <igk@igk.ru>
2019-06-09 17:44:07 +0300

    server production

---
+++ src/server_prod.mjs
@@ -0,0 +1,24 @@
+/* jshint esversion: 6 */
+/* jshint asi: true */
+/* jshint node: true */
+/* jshint varstmt: true */
+
+'use strict'
+
+import fs from 'fs'
+import path from 'path'
+import {Agent} from 'https'
+import httpProxy from 'http-proxy-middleware'
+
+
+const agent = Agent({
+	keepAlive: true,
+})
+
+httpProxy('/api', {
+  pathRewrite: {'^/api' : ''},
+  target: 'https://jsonplaceholder.typicode.com/todos/1',
+  changeOrigin: true,
+  agent: agent,
+  logLevel: 'error',
+}),
